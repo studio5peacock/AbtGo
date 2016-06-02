b3e9e3cf033e8b43a5d62ac22a20f3c80e331e96
Sophie You <jlyou@hotmail.com>
2016-06-02 07:51:37 -0400

    Initial Commit

---
+++ AbtGo/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  AbtGo
+//
+//  Created by Genming Zhao on 2016-06-02.
+//  Copyright © 2016 Genming Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ AbtGoTests/AbtGoTests.m
@@ -0,0 +1,39 @@
+//
+//  AbtGoTests.m
+//  AbtGoTests
+//
+//  Created by Genming Zhao on 2016-06-02.
+//  Copyright © 2016 Genming Zhao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AbtGoTests : XCTestCase
+
+@end
+
+@implementation AbtGoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
